f53d47f6d726a5a65535c28fb675047655e8cae7
Salohy <najorosalohy1@gmail.com>
2024-04-01 22:04:14 +0200

    Extrayez les paramètres affichés de la méthode précédente pour les placer dans une méthode

--- src/iut/bad/Humain.java
+++ src/iut/bad/Humain.java
@@ -1,5 +1,12 @@
 package iut.bad;
 
 public class Humain {
-
+	 protected String nom;
+	    protected String prenom;
+	    protected int age;
+	    
+	    @Override
+	    public String toString() {
+	        return "Nom: " + nom + ", Prénom: " + prenom + ", Âge: " + age;
+	    }
 }
